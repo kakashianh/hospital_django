62b3c65c56e7a6e22d0e5d86e73860faef17ad1b
Thiện Nhân <23730110@ms.uit.edu.vn>
2025-06-10 10:37:26 +0700

    Tạo crud api cho physicians

--- hospital_api/urls.py
+++ hospital_api/urls.py
@@ -1,9 +1,10 @@
-# todo/todo_api/urls.py : API urls.py
 from django.urls import path, include
-from .views import (
-    PhysicianListApiView,
-)
+from rest_framework.routers import DefaultRouter
+from .views import PhysicianViewSet
+
+router = DefaultRouter()
+router.register(r'physicians', PhysicianViewSet)
 
 urlpatterns = [
-    path('api', PhysicianListApiView.as_view()),
+    path('', include(router.urls)),
 ]
